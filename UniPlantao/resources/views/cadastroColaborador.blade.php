d7e3a8b553a07b23062c0c7bebe497a2e6966c8a
Daniel Vitor <daniel.pereira@edu.unifil.br>
2022-05-21 11:22:13 -0300

    teste Commit

---
+++ UniPlantao/resources/views/cadastroColaborador.blade.php
@@ -0,0 +1,12 @@
+<h1>Cadastro Colaboradores</h1>
+<a href="/">Voltar para Home</a>
+
+
+
+<div>
+            <form action="/cadastroColaborador" method="POST">
+                <Label>Nome</Label>
+                <input type="Text" name="nome">
+                <button>Confirma</button>
+            </form>
+        </div>
---
+++ UniPlantao/resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Teste 123 456</h1>
+<a href="/">Voltar para Home</a>
